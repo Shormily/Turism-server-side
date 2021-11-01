8146271513aa75d65be64101aa341d4d76c61ad9
Shormily <86643195+Shormily@users.noreply.github.com>
2021-11-01 20:38:16 +0600

    my orders

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ async function run() {
       res.json(order);
     });
 
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { userEmail: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+
     // POST API
     app.post("/services", async (req, res) => {
       const service = req.body;
