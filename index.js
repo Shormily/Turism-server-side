3da9989e09ad5b5af59b06d927a4f3fb3f9dad24
Shormily <86643195+Shormily@users.noreply.github.com>
2021-11-01 20:16:04 +0600

    put

--- index.js
+++ index.js
@@ -63,6 +63,25 @@ async function run() {
       const result = await orderCollection.insertOne(order);
       res.json(result);
     });
+
+    // PUT / Update orders status api
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedOrder = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updatedOrder.status,
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
   } finally {
     // await client.close()
   }
