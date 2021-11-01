10376c9e8baecef9472657342d67ac6194e7c3b6
Shormily <86643195+Shormily@users.noreply.github.com>
2021-11-01 20:43:58 +0600

    delete

--- index.js
+++ index.js
@@ -91,6 +91,13 @@ async function run() {
       );
       res.json(result);
     });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close()
   }
